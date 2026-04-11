9d12e901aaebcccbb0965b4087358db2bc26af47
Reinhold2005 <ndevahomareinhold@gmail.com>
2026-04-11 17:58:46 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,18 +1,18 @@
 <?php
-<?php
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Add this to help identify permission issues
+if (!is_writable(storage_path())) {
+    die('Storage directory is not writable. Path: ' . storage_path());
+}
+
 define('LARAVEL_START', microtime(true));
-
-
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
